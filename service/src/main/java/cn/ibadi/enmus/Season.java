0fd06ec079e120a6b72faf5ae2fe795ad760d887
jhonRawls <jhonRawls@gmial.com>
2017-05-28 20:43:39 +0800

    test enmus

---
+++ service/src/main/java/cn/ibadi/enmus/Season.java
@@ -0,0 +1,8 @@
+package cn.ibadi.enmus;
+
+public enum Season {
+	SPRING,SUMMER,AUTUMN,WINTER;
+	public static void main(String [] args){
+		System.out.println("main method");
+	}
+}
--- service/src/main/java/cn/ibadi/service/App.java
+++ service/src/main/java/cn/ibadi/service/App.java
@@ -1,13 +1,17 @@
 package cn.ibadi.service;
 
+import cn.ibadi.enmus.Season;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		for (Season season : Season.values()) {
+			System.out.println(season.name());
+		}
+		
+		System.out.println("Hello World!");
+	}
 }
