a6ef9ca989182746aefbf0c8facb7227e41106e2
jhonRawls <jhonRawls@gmial.com>
2017-05-29 11:49:47 +0800

    add change method

--- service/src/main/java/cn/ibadi/service/App.java
+++ service/src/main/java/cn/ibadi/service/App.java
@@ -11,7 +11,29 @@ public class App {
 		for (Season season : Season.values()) {
 			System.out.println(season.name());
 		}
-		
+		System.out.println("---------------");
+		change();
 		System.out.println("Hello World!");
+	}
+
+	public static void change() {
+		Season season = Season.SUMMER;
+		switch (season) {
+		case SPRING:
+			season = Season.SUMMER;
+			break;
+		case SUMMER:
+			season = Season.AUTUMN;
+			break;
+		case AUTUMN:
+			season = Season.WINTER;
+			break;
+		case WINTER:
+			season = Season.SPRING;
+			break;
+		default:
+			break;
+		}
+		System.out.println(season.ordinal()+season.name());
 	}
 }
