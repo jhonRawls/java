b978384f5c6333f1a1eb616d381be486d555b17b
jhonRawls <jhonRawls@gmial.com>
2017-07-22 22:03:43 +0800

    commit something

---
+++ service/src/test/java/cn/ibadi/test/TestA.java
@@ -0,0 +1,10 @@
+package cn.ibadi.test;
+
+public class TestA {
+	static String name;
+	static {
+		name = "test";
+		System.out.println("执行....");
+	}
+
+}
---
+++ service/src/test/java/cn/ibadi/test/TestB.java
@@ -0,0 +1,10 @@
+package cn.ibadi.test;
+
+public class TestB {
+	public static void main(String [] args) {
+		TestA a=new TestA();
+		TestA ab=new TestA();
+		TestA abc=new TestA();
+		
+	}
+}
